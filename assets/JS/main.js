94f3432a6875fa9805a2948651c067249d0a4292
Francesca Golzio <francescagolzio@gmail.com>
2026-02-11 18:37:45 +0100

    add switch on/of
    bonus

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -9,24 +9,60 @@ const btnEl = document.getElementById('btn');
 
 /* Switch on 💡 */
 
-btnEl.addEventListener('click', function() {
+// btnEl.addEventListener('click', function() {
 
-//console.log(btnEl.classList);
-btnEl.innerHTML = 'Spegni';
+//   /* changes on button */
+//   //console.log(btnEl.classList);
+//   btnEl.innerHTML = 'Spegni';
+  
+//   /* changes on bulb img */
+// //const bulbProperties = bulbImgEl.classList;
+// //console.log(bulbProperties);
+// bulbImgEl.style.transition = 'all 2s';
+// bulbImgEl.src = './assets/img/yellow_lamp.png';
+// bulbImgEl.alt = 'lampadina accesa';
 
-//const bulbProperties = bulbImgEl.classList;
-//console.log(bulbProperties);
-bulbImgEl.style.transition = 'all 2s';
-bulbImgEl.src = './assets/img/yellow_lamp.png';
-bulbImgEl.alt = 'lampadina accesa';
-//bulbImgEl.style.transformOrigin = '-1 0'; //NON funziona...
+// })
+
+
+/* BONUS */
+
+btnEl.addEventListener('click', function() {  
+
+  const buttonLabel = btnEl.innerHTML;
+  console.log(buttonLabel);
+
+  /* Switch on 💡 */
+  if(buttonLabel === 'Accendi') {
+    
+    /* changes on button */
+    //console.log(btnEl.classList);
+    btnEl.innerHTML = 'Spegni';
+    
+    /* changes on bulb img */
+    //const bulbProperties = bulbImgEl.classList;
+    //console.log(bulbProperties);
+    bulbImgEl.style.transition = 'all 2s';
+    bulbImgEl.src = './assets/img/yellow_lamp.png';
+    bulbImgEl.alt = 'lampadina accesa';
+  }
+  
+  /* Switch off 📴 */
+  if(buttonLabel === 'Spegni') {    
+
+    /* changes on button */
+    //console.log(btnEl.classList);
+    btnEl.innerHTML = 'Accendi';
+
+    /* changes on bulb img */
+    //const bulbProperties = bulbImgEl.classList;
+    //console.log(bulbProperties);
+    bulbImgEl.style.transition = 'all 2s';
+    bulbImgEl.src = './assets/img/white_lamp.png';
+    bulbImgEl.alt = 'lampadina spenta';
+  }
 
 })
-
-
-
-
-
 
 
 
