1c7227e70a8c56a871195d502ff35a6ac0bbea27
Francesca Golzio <francescagolzio@gmail.com>
2026-02-11 16:41:47 +0100

    add to bulb switch on effect

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,11 +1,25 @@
 /* is it working? */
 console.log("🟢 it's working...")
 
+/* Collect */
+const bulbImgEl = document.getElementById('bulbImg');
+const btnEl = document.getElementById('btn');
+
+//console.log(bulbEl, btnEl);
+
+/* Switch on 💡 */
+
+btnEl.addEventListener('click', function() {
 
 
 
+//const bulbProperties = bulbImgEl.classList;
+//console.log(bulbProperties);
+bulbImgEl.src = './assets/img/yellow_lamp.png';
+bulbImgEl.alt = 'lampadina accesa';
+//bulbImgEl.style.transformOrigin = '-1 0'; //NON funziona...
 
-
+})
 
 
 
